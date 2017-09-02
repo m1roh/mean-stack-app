1cc959d5af9db7ad81dd7c7a86a49808961d20b5
m1roh <hayromain.rh@gmail.com>
2017-09-02 10:18:05 +0200

    Add routers and controllers to post new hotels and reviews : updated reviews.controller and hotels.controllers

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -73,3 +73,58 @@ module.exports.reviewsGetOne = function(req, res) {
             .json(response.message);
     });
 };
+
+var _addReview = function(req, res, hotel) {
+
+    hotel.reviews.push({
+        name : req.body.name,
+        rating : parseInt(req.body.rating, 10),
+        review : req.body.review
+    });
+
+    hotel.save(function(err, hotelUpdated) {
+        if (err) {
+            res
+                .status(500)
+                .json(err);
+        } else {
+            res
+                .status(200)
+                .json(hotelUpdated.reviews[hotelUpdated.reviews.length - 1]);
+        }
+    });
+};
+
+module.exports.reviewsAddOne = function(req, res) {
+
+    var id = req.params.hotelId;
+    console.log("GET hotelId", id);
+
+    Hotel
+        .findById(id)
+        .select('reviews')
+        .exec(function(err, doc) {
+            var response = {
+                status : 200,
+                message : doc
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } else if (!doc) {
+                console.log("Hotel not found in database", id);
+                response.status = 404;
+                response.message = {
+                    "message" : "Hotel ID not found " + id
+                };
+            } 
+            if (doc) {
+                _addReview(req, res, doc);
+            } else {
+                res 
+                    .status(response.status)
+                    .json(response.message);
+            }
+        })
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,25 +4,25 @@ var router = express.Router();
 var ctrlHotels = require('../controllers/hotels.controllers.js');
 var ctrlReviews = require('../controllers/reviews.controllers.js');
 
+// Hotel routes
 router
-    .route('/hotels')
-    .get(ctrlHotels.hotelsGetAll);
+  .route('/hotels')
+  .get(ctrlHotels.hotelsGetAll)
+  .post(ctrlHotels.hotelsAddOne);
 
 router
-    .route('/hotels/:hotelId')
-    .get(ctrlHotels.hotelsGetOne);
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne);
 
-router
-    .route('/hotels/new')
-    .post(ctrlHotels.hotelsAddOne);
 
 // Review routes
 router
-.route('/hotels/:hotelId/reviews')
-.get(ctrlReviews.reviewsGetAll);
+  .route('/hotels/:hotelId/reviews')
+  .get(ctrlReviews.reviewsGetAll)
+  .post(ctrlReviews.reviewsAddOne);
 
 router
-.route('/hotels/:hotelId/reviews/:reviewId')
-.get(ctrlReviews.reviewsGetOne);
+  .route('/hotels/:hotelId/reviews/:reviewId')
+  .get(ctrlReviews.reviewsGetOne);
 
 module.exports = router;
