ed0df0bad80d8eee90052802e11d76b591c4caa4
m1roh <hayromain.rh@gmail.com>
2017-09-02 08:35:19 +0200

    Added error trapping to controllers : updated reviews.controllers and hotels.controllers

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -6,6 +6,15 @@ var runGeoQuery = function(req, res) {
     var lng = parseFloat(req.query.lng);
     var lat = parseFloat(req.query.lat);
 
+    if (isNaN(lng) || isNaN(lat)) {
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied en querystring, lng and lat must both be numbers"
+            });
+        return;
+    }
+
     // A geoJSON point
     var point = {
         type : "Point",
@@ -22,9 +31,16 @@ var runGeoQuery = function(req, res) {
         .geoNear(point, geoOptions, function(err, results, stats) {
             console.log('Geo results', results);
             console.log('Geo stats', stats);
-            res
-                .status(200)
-                .json(results);
+            if (err) {
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            } else {
+                res
+                    .status(200)
+                    .json(results);
+            }           
         });
 };
 
@@ -32,6 +48,7 @@ module.exports.hotelsGetAll = function(req, res) {
 
     var offset = 0;
     var count = 5;
+    var maxCount = 10;
 
     if (req.query && req.query.lat && req.query.lng) {
         runGeoQuery(req, res);
@@ -46,14 +63,39 @@ module.exports.hotelsGetAll = function(req, res) {
         count = parseInt(req.query.count, 10);
     }
 
+    if (isNaN(offset) || isNaN(count)) {
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring count and offset should be numbers"
+            });
+        return;
+    }
+
+    if (count > maxCount) {
+        res 
+            .status(400)
+            .json({
+                "message" : "Count limit of " + maxCount + " exceeded"
+            });
+        return;
+    }
+
     Hotel
         .find()
         .skip(offset)
         .limit(count)
         .exec(function(err, hotels) {
-            console.log("Found hotels", hotels.length);
-            res
-                .json(hotels);
+            if (err) {
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            } else {
+                console.log("Found hotels", hotels.length);
+                res
+                    .json(hotels);
+            }
         });
 };
 
@@ -64,9 +106,23 @@ module.exports.hotelsGetOne = function(req, res) {
     Hotel
         .findById(hotelId)
         .exec(function(err, doc) {
+            var response = {
+                status : 200,
+                message : doc
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500
+                response.message = err;
+            } else if (!doc) {
+                response.status = 404;
+                response.message = {
+                    "message" : "Hotel ID not found"
+                };
+            }
             res
-                .status(200)
-                .json(doc);
+                .status(response.status)
+                .json(response.message);   
         });
 };
 
--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -3,17 +3,33 @@ var Hotel = mongoose.model('Hotel');
 
 // GET all reviews for a hotel
 module.exports.reviewsGetAll = function(req, res) {
-    var hotelId = req.params.hotelId;
-    console.log("GET hotelId", hotelId); 
+    var id = req.params.hotelId;
+    console.log("GET reviews for hotelId", id); 
 
     Hotel
-        .findById(hotelId)
+        .findById(id)
         .select('reviews')
         .exec(function(err, doc) {
-            console.log("Returned doc", doc);
+            var response = {
+                status : 200,
+                message : []
+            };
+            if (err) {
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } else if (!doc) {
+                console.log("Hotel id not found in database", id);
+                response.status = 404;
+                response.message = {
+                    "message" : "Hotel ID not found " + id
+                };
+            } else {
+                response.message = doc.reviews ? doc.reviews : [];
+            }
             res
-                .status(200)
-                .json(doc.reviews);
+                .status(response.status)
+                .json(response.message);
         });
 };
 
@@ -27,10 +43,33 @@ module.exports.reviewsGetOne = function(req, res) {
     .findById(hotelId)
     .select('reviews')
     .exec(function(err, hotel) {
-        console.log("Returned hotel", hotel);
-        var review = hotel.reviews.id(reviewId);
+        var response = {
+            status : 200,
+            message : {}
+        };
+        if (err) {
+            console.log("Error finding hotel");
+            response.status = 500;
+            response.message = err;
+        } else if (!hotel) {
+            console.log("Hotel id not found in database", id);
+            response.status = 404;
+            response.message = {
+                "message" : "Hotel ID not found " + id
+            };
+        } else {
+            // GET the review
+            response.message = hotel.reviews.id(reviewId);
+            // If the review doesn't exist Mongoose return null
+            if (!response.message) {
+                response.status = 404;
+                response.message = {
+                    "message" : "Review ID not found " + reviewId
+                };
+            }
+        }
         res
-            .status(200)
-            .json(review);
+            .status(response.status)
+            .json(response.message);
     });
 };
