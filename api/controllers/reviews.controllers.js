c13581f976cd9bc1b006a1761f3c1b7fa9b3f97b
m1roh <hayromain.rh@gmail.com>
2017-09-02 14:03:20 +0200

    Added delete methods to the routes and controllers -> updated : hotels.controllers, reviews.controllers, index.js

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -186,3 +186,59 @@ module.exports.reviewsUpdateOne = function(req, res) {
         }
     });
 };
+
+module.exports.reviewsDeleteOne = function(req, res) {
+    var hotelId = req.params.hotelId;
+    var reviewId = req.params.reviewId;
+    console.log("PUT reviewId " + reviewId + " for hotelId " + hotelId);
+
+    Hotel
+    .findById(hotelId)
+    .select('reviews')
+    .exec(function(err, hotel) {
+        var response = {
+            status : 200,
+            message : {}
+        };
+        if (err) {
+            console.log("Error finding hotel");
+            response.status = 500;
+            response.message = err;
+        } else if (!hotel) {
+            console.log("Hotel id not found in database", hotelId);
+            response.status = 404;
+            response.message = {
+                "message" : "Hotel ID not found " + hotelId
+            };
+        } else {
+            // GET the review
+            thisReview = hotel.reviews.id(reviewId);
+            // If the review doesn't exist Mongoose return null
+            if (!thisReview) {
+                response.status = 404;
+                response.message = {
+                    "message" : "Review ID not found " + reviewId
+                };
+            }
+        }
+        if (response.status !== 200) {
+            res
+                .status(response.status)
+                .json(response.message);
+        } else {
+            hotel.reviews.id(reviewId).remove();
+
+            hotel.save(function(err, hotelUpdated) {
+                if (err) {
+                    res
+                        .status(500)
+                        .json(err);
+                } else {
+                    res 
+                        .status(204)
+                        .json();
+                }
+            });
+        }
+    });
+}
--- api/routes/index.js
+++ api/routes/index.js
@@ -6,25 +6,27 @@ var ctrlReviews = require('../controllers/reviews.controllers.js');
 
 // Hotel routes
 router
-  .route('/hotels')
-  .get(ctrlHotels.hotelsGetAll)
-  .post(ctrlHotels.hotelsAddOne);
+    .route('/hotels')
+    .get(ctrlHotels.hotelsGetAll)
+    .post(ctrlHotels.hotelsAddOne);
 
 router
-  .route('/hotels/:hotelId')
-  .get(ctrlHotels.hotelsGetOne)
-  .put(ctrlHotels.hotelsUpdateOne);
+    .route('/hotels/:hotelId')
+    .get(ctrlHotels.hotelsGetOne)
+    .put(ctrlHotels.hotelsUpdateOne)
+    .delete(ctrlHotels.hotelsDeleteOne);
 
 
 // Review routes
 router
-  .route('/hotels/:hotelId/reviews')
-  .get(ctrlReviews.reviewsGetAll)
-  .post(ctrlReviews.reviewsAddOne);
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll)
+    .post(ctrlReviews.reviewsAddOne);
 
 router
-  .route('/hotels/:hotelId/reviews/:reviewId')
-  .get(ctrlReviews.reviewsGetOne)
-  .put(ctrlReviews.reviewsUpdateOne);
+    .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne)
+    .put(ctrlReviews.reviewsUpdateOne)
+    .delete(ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
